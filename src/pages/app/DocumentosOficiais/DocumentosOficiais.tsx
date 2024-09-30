0f928d7bd3829da748ce36560cbe865476f0c6b6
Eduardo Lopes da Silva Meireles <edwardmeireles@icloud.com>
2024-09-29 21:20:57 -0300

    implementando pagina de Sistemad de Doacoes

--- src/pages/app/DocumentosOficiais/DocumentosOficiais.tsx
+++ src/pages/app/DocumentosOficiais/DocumentosOficiais.tsx
@@ -8,27 +8,29 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { Card, CardContent } from "@/components/ui/card"
+import { ImagePlus } from "lucide-react";
 
 
 export function DocumentosOficiais(){
   return (
     <div>
       <Helmet title="DocumentosOficiais" />
-      <div className="flex flex-col items-center justify-center text-justify w-full">
+      {/* Conteúdo da H1*/}
+      <div className="flex flex-col items-center justify-center text-justify w-full mb-5">
         <p className="text-3xl font-bold">Documentos oficiais</p>
         <p className="opacity-50 text-sm font-medium">
           Aqui você tem acesso a todos os documentos oficiais 
           em que o parlamentar emitiu ou participou.
         </p>
       </div>
-
-      <div className="flex items-center justify-center h-screen">
-        <Carousel className="flex flex-col w-screen h-screen">
+        {/* Carousel component/content */}
+      <div className="flex items-center justify-center h-auto p-6 mb-24 ">
+        <Carousel className="flex flex-col w-auto h-auto ">
           <CarouselContent className="-ml-1">
             {Array.from({ length: 5 }).map((_, index) => (
               <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
                 <div className="p-3">
-                  <Card>
+                  <Card className="bg-[#4a4a4a]">
                     <CardContent className="flex aspect-square items-center justify-center p-6">
                       <span className="text-2xl font-semibold">conteúdo {index + 1}</span>
                     </CardContent>
@@ -37,33 +39,101 @@ export function DocumentosOficiais(){
               </CarouselItem>
             ))}
           </CarouselContent>
+          <CarouselPrevious className="absolute top-1/2 left-4 transform -translate-y-1/2" />
+          <CarouselNext className="absolute top-1/2 right-4 transform -translate-y-1/2" />
         </Carousel>
-      </div>
-      <div className="flex items-center space-x-4">
-        <div className="w-[343px] h-[318px] bg-[#4a4a4a] rounded-[15px]">
-
-        </div>
-
-        <div className="space-y-6">
-          <div>
-            <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
-              <span>content message user</span> 
-            </div>
-          </div>
-
-          <div>
-            <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
-              <span>content message user</span> 
-            </div>
-          </div>
-
-          <div>
-            <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
-              <span>content message user</span> 
-            </div>
-          </div>
-        </div>
         
+      </div>
+      {/* Conteúdo da primeira box */}
+      <div className="flex flex-wrap mb-5">
+        <div id="firstBox" className="flex items-center space-x-4 mb-5 p-10">
+          <div className="w-[343px] h-[318px] bg-[#4a4a4a] rounded-[15px] flex justify-center items-center">
+            <ImagePlus className="w-[70px] h-[69px]" />
+          </div>
+
+
+          <div className="space-y-6">
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      {/* Conteúdo da segunda box */}
+        <div className="flex justify-end w-full">
+
+          <div id="secondBox" className="flex items-center space-x-4 mb-5 p-10 ml-auto">
+            <div className="space-y-6">
+              <div>
+                <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                  <span>content message user</span>
+                </div>
+              </div>
+
+              <div>
+                <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                  <span>content message user</span>
+                </div>
+              </div>
+              <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+            <div>
+              <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                <span>content message user</span> 
+              </div>
+            </div>
+
+              <div>
+                <div className="text-center w-[645px] h-5 bg-gradient-to-r from-[#4a4a4a] via-[#4a4a4a] to-[#b0b0b0] rounded-[20px]">
+                  <span>content message user</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Container cinza maior à direita */}
+            <div className="w-[343px] h-[318px] bg-[#4a4a4a] rounded-[15px] flex justify-center items-center">
+              <ImagePlus className="w-[70px] h-[69px]" />
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
--- src/pages/app/SistemaDoacoes/SistemaDoacoes.tsx
+++ src/pages/app/SistemaDoacoes/SistemaDoacoes.tsx
@@ -1,13 +1,37 @@
+import { Image } from "lucide-react"
 import { Helmet } from "react-helmet-async"
 
 export function SistemaDoacoes(){
   return (
     <div>
       <Helmet title="SistemaDoacoes" />
-      <div className="text-black text-3xl font-bold font-['Inter']">Documentos oficiais </div>
-      <div className="opacity-50 text-black text-xs font-medium font-['Inter']">Aqui você tem acesso a todos os documentos oficiais <br/>em que o parlamentar emitiu ou participou. </div>
-      <div className="w-[261px] h-[318px] bg-[#4a4a4a] rounded-[15px]" />
-      <div className="w-[343px] h-[318px] bg-[#4a4a4a] rounded-[15px]" />
+      <div className="flex flex-col items-center justify-center text-justify w-full p-2 mb-24"> 
+            <p className="text-3xl font-bold">
+            Sistema de doações
+            </p>
+            <p className="opacity-50 text-sm font-medium">
+            Navegue por região e fique por dentro das nossas
+            atividades em seu município.
+            </p>
+          </div>
+
+            <div className="w-full h-[479px] bg-[#4a4a4a] flex justify-center items-end relative">
+              <div className="w-[418px] h-[548.17px] bg-[#c4c4c4]">
+                {/* Conteúdo da div cinza */}
+              </div>
+            <div>
+              
+            </div>
+            <div className="absolute bottom-[-150px] w-[1170px] h-[161px] bg-white rounded-[10px] transform -translate-y-[80px] flex items-center space-x-12 p-6">
+  {/* Conteúdo da div branca */}
+              <div className="flex items-center justify-center w-[90px] h-[90px] bg-[#4a4a4a] rounded-full">
+                <Image />
+              </div>
+              <div className="flex items-center justify-center w-[90px] h-[90px] bg-[#4a4a4a] rounded-full">
+                <Image />
+              </div>
+            </div>
+          </div> 
     </div>
   )
 }
